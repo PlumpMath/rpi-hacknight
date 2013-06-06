ae35e51a3f1b335e6de5642ba25e36007478770a
luc <luc@spreadshirt.net>
2013-06-06 21:39:00 +0200

    More load time (list & api search).

--- are-we-up/jobs/load_time.rb
+++ are-we-up/jobs/load_time.rb
@@ -20,13 +20,31 @@ class Time
   end
 end
 
-last_time = 0
-SCHEDULER.every '2s' do |j|
-  st = Time.new.to_ms
-  res = get_redirected(URI('http://spreadshirt.de/-C4407'))
-  fi = Time.new.to_ms
+class TimedRequest
+  @@req_times = {}
 
-  time = fi - st
-  send_event('sprd.index.load', { current: time, last: last_time })
-  last_time = time
+  def self.get(widget, url)
+    st = Time.new.to_ms
+    res = get_redirected(URI(url))
+    fi = Time.new.to_ms
+
+    time = fi - st
+    send_event(widget, { current: time, last: @@req_times[widget] || 0 })
+    @@req_times[widget] = time
+  end
+end
+
+class Dict
+  @@dict = File.read('/usr/share/dict/words').lines.map {|l| l[0..-2]}
+
+  def self.random
+    @@dict[rand() * (@@dict.length - 1)]
+  end
+end
+
+SCHEDULER.every '1s' do |j|
+  TimedRequest.get('sprd.index.load', 'http://spreadshirt.de')
+  TimedRequest.get('sprd.list.load', 'http://spreadshirt.de/-C4407')
+
+  TimedRequest.get('sprd.api.search', "http://api.spreadshirt.net/api/v1/shops/205909/articles?query=#{Dict.random}")
 end
